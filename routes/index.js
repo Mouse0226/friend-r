e8dc413e2dd5ba03ed56dd9b875740b97bfc573e
Mouse0226 <mouse1987.ml@gmail.com>
2022-04-23 19:04:48 -0500

    Added shell routes

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const thoughtRoutes = require('./thought-routes');
+const userRoutes = require('./user-routes');
+
+router.use('/thoughts', thoughtRoutes);
+router.use('/users', userRoutes);
+
+router.use((req, res) => {
+    res.status(404).send('<h1>404 Not Found!</h1>');
+});
+
+module.exports = router;
